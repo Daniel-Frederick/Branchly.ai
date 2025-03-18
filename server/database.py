4f8749b24128dd356b6e8c018834b2cf3b066e26
Daniel-Frederick <frederickd5@udayton.edu>
2025-03-19 01:29:16 +0700

    database folder

--- server/database.py
+++ server/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-# Can I put the database files in a database folder?
-DATABASE_URL = "sqlite:///user.db"
+os.makedirs("database", exist_ok=True)
+DATABASE_URL = "sqlite:///database/user.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
